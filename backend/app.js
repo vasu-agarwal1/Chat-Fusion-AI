29a6c2abb391998c32dce006dae59306c51ae3e8
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-11 17:40:04 +0530

    added routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import connect from './db/db.js'
+import userRoutes from './routes/user.routes.js'
 
 connect()
 
@@ -9,6 +10,8 @@ const app = express()
 app.use(morgan('dev'))//with this we can get the info. of which route has been clicked
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/users', userRoutes)
 
 
 app.get('/', (req, res) => {
