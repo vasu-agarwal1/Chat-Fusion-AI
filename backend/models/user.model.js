b00ff6616a14e2971c36310bd5fad255989315d8
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-11 17:36:38 +0530

    created user module for db schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: [6, 'Email must be at least 6 chracters long'],
+        maxLength: [50, 'Email must not be longer than 50 characters']
+    },
+
+    password: {
+        type: String,
+        select: false,
+    }
+})
+
+userSchema.statics.hashPassword = async function(password) {
+    return await bcrypt.hash(password, 10)
+}
+
+userSchema.methods.isValidPassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateJWT = function () {
+     return jwt.sign({email: this.email }, process.env.JWT_SECRET)
+}
+
+const user = mongoose.model('user', userSchema)
+
+export default user
