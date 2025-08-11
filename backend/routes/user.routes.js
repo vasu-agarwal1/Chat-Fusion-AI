9047d53dbfe76db2bfd2524b2c173d030d554fba
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-11 17:39:01 +0530

    created routes file to define end points

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import * as userController from '../controllers/user.controller.js'
+import { body } from "express-validator";
+
+const router = Router()
+
+
+router.post('/register',
+    body('email').isEmail().withMessage('Email must be a valid email address'),
+    body('password').isLength({min: 3}).withMessage('password must be atleast 3 characters long'),
+
+    userController.createUserController)
+
+    router.post('/login', 
+        body('email').isEmail().withMessage('Email must be a valid email address'),
+        body('password').isLength({min: 3}).withMessage('Password must be atLeast 3 characters long'),
+        userController.loginController
+    )
+
+
+export default router
